bcbb06ee0eb88077b011b6be7a4ed1406cd239e4
zethra <jediben97@gmail.com>
2017-12-26 16:40:06 -0500

    Added directory listing for directories

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,91 @@ use std::io;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::path::Path;
 
 use futures::future;
 use tokio_minihttp::{Request, Response, Http};
 use tokio_proto::TcpServer;
 use tokio_service::Service;
 
-struct HelloWorld;
+struct Server;
 
-impl Service for HelloWorld {
+impl Service for Server {
     type Request = Request;
     type Response = Response;
     type Error = io::Error;
     type Future = future::Ok<Response, io::Error>;
 
     fn call(&self, request: Request) -> Self::Future {
+//        println!("path: {}", request.path());
         let mut resp = Response::new();
-        let file = match File::open(".".to_string() + request.path()) {
-            Ok(file) => file,
-            Err(_) => {
-                resp.status_code(404, "File not found");
-                resp.body("File not found");
+        let path_str = ".".to_string() + request.path();
+        let path = Path::new(&path_str);
+        println!("current path: {:?}", path);
+        if path.is_dir() {
+            match path.read_dir() {
+                Ok(dir) => {
+                    let mut page = String::new();
+                    page.push_str(r#"
+<html>
+<head>
+    <title>Directory listing</title>
+</head>
+<body>
+    <h1>Directory listing</h1>
+    <ul>
+"#);
+                    for item in dir {
+                        match item {
+                            Ok(item) => {
+                                println!("item: {:?}", item);
+                                let path = item.path();
+                                println!("path: {:?}", path);
+                                let mut path_str = path.to_string_lossy().to_string();
+                                path_str.remove(0);
+                                println!("path_str: {:?}", path_str);
+                                let name = item.file_name();
+                                let mut name_str = name.to_string_lossy().to_string();
+                                if path.is_dir() {
+                                    name_str.push('/');
+                                }
+                                page.push_str(format!("\t\t<li><a href=\"{}\">{}</a></li>\n", path_str, name_str).as_str());
+                            }
+                            Err(e) => {
+                                println!("{}", e);
+                            }
+                        }
+                    }
+                    page.push_str(r#"
+    </ul>
+</body>
+</html>
+                    "#);
+                    resp.body(page.as_str());
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        } else {
+            let file = match File::open(path) {
+                Ok(file) => file,
+                Err(_) => {
+                    resp.status_code(404, "File not found");
+                    resp.body("File norust check it found");
+                    return future::ok(resp);
+                }
+            };
+            let mut buf_reader = BufReader::new(file);
+            let mut content = Vec::new();
+            if let Err(e) = buf_reader.read_to_end(&mut content) {
+                resp.status_code(500, "Internal error");
+                resp.body("Internal error");
+                println!("{}", e);
                 return future::ok(resp);
             }
-        };
-        let mut buf_reader = BufReader::new(file);
-        let mut content = Vec::new();
-        if let Err(e) = buf_reader.read_to_end(&mut content) {
-            resp.status_code(500, "Internal error");
-            resp.body("Internal error");
-            println!("{}", e);
-            return future::ok(resp);
+            resp.body_bytes(content.as_slice());
         }
-        resp.body_bytes(content.as_slice());
         future::ok(resp)
     }
 }
@@ -48,5 +100,5 @@ fn main() {
     let addr = "[::1]:8000".parse().unwrap();
     println!("Starting server on http://[::1]:8000");
     TcpServer::new(Http, addr)
-        .serve(|| Ok(HelloWorld));
+        .serve(|| Ok(Server));
 }
